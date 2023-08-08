9e0adb51239e10cfbceabc6941ed3b00ecc4ec61
Kaviyapcse <kaviyap1804@gmail.com>
2023-08-08 15:35:40 +0530

    Deleted the commented lines

--- crud_app/admin.py
+++ crud_app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Label
 
 # Register your models here.
-@admin.register(Label)
-class LabelAdmin(admin.ModelAdmin):
-    data=['name','color','description']
